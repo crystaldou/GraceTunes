0b6f4029e4e4a186b419fd31aaf0c02416ae9981
Crystal Dou <Crystal@Crystals-MacBook-Pro.local>
2013-10-07 19:16:20 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GraceTunes::Application.config.secret_token = '079fd5e750e08f39141ba526f9750d92141ea57a99a345e4cdf2ce19cfc0ad818ae7307e3abd334630a282690b3eda1d3ea0556115a35fc468481095cafeac4b'
