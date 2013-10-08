aac936692fc60b9f0341f06715cbce35593238a2
James Han <james.han92@berkeley.edu>
2013-10-07 22:37:41 -0700

    added new rails app

---
+++ gracetunes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gracetunes::Application.config.secret_token = '223e2961c42d156d1eb71fec3c51bb1ffe7be6bb51c871966b84f090a484f2652ef301e6ba46d142d4131c8ddcb3c5cc8f81fafa68ed49af813719b1e4352b27'
