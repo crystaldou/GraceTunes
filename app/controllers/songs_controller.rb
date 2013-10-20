2b8e47c9042e14da5ebcb4b8494f1d7d9793e2c7
qkw24 <qkw2006@gmail.com>
2013-10-19 17:05:08 -0700

    added songs controller v1

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,8 @@
 class SongsController < ApplicationController
 
   def show
+    id = params[:id] # retrieve movie ID from URI route
+    @song = Songs.find(id) # look up movie by unique ID
     
   end
 
@@ -13,19 +15,27 @@ class SongsController < ApplicationController
   end
 
   def create
-    
+    @song = Songs.create!(params[:song])
+    flash[:notice] = "#{@song.title} was successfully created."
+    redirect_to songs_path
   end
 
   def edit
-    
+    @song = Songs.find params[:id]
   end
 
   def update
-    
+    @song = Songs.find params[:id]
+    @song.update_attributes!(params[:movie])
+    flash[:notice] = "#{@song.title} was successfully updated."
+    redirect_to songs_path(@song)
   end
 
   def destroy
-    
+    @song = Songs.find(params[:id])
+    @song.destroy
+    flash[:notice] = "Successfully removed '#{@song.title}' "
+    redirect_to songs_path
   end
 
 
