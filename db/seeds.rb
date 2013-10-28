2fb6fcc457a79f4f80a83fb5eb702bb4d5bc9361
James Han <james.han92@berkeley.edu>
2013-10-27 22:31:09 -0700

    seeds edited

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,18 @@
 
 songs = [{:title => 'Your Love Never Fails', :artist => 'Jesus Culture', :album => 'From The Inside Out', :tags => 'love, never fails'},
       	  {:title => 'Give Me Faith', :artist => 'Elevation Worship', :album => 'Kingdom Come', :tags => 'faith, spirit, perseverance'},
+  	  {:title => 'From the Inside Out', :artist => 'Hillsong United', :album => 'United We Stand', :tags => 'transformation, changed lives, failures'},
+  	  
+  	  {:title => 'Glorious Day', :artist => 'Casting Crowns', :album => 'Until The Whole World Hears', :tags => 'salvation, rapture, where\'d everyone go?'},
+  	  {:title => 'Build Your Kingdom Here', :artist => 'Rend Collectiv e Experiment', :album => 'Homemade Worship By Handmade Peopple', :tags => 'gospel, kingdom, church, on fire'},
+  	  {:title => 'Desert Soul', :artist => 'Rend Collective Experiment', :album => 'Homemade Worship By Handmade Peopple', :tags => 'dead, burnt out, perseverance'},
+  	  {:title => '10,000 Reasons', :artist => 'Matt Redman', :album => '10,000 Reasons', :tags => 'blessings, worship, holy'},
+  	  {:title => 'The Heart of Worship', :artist => 'Matt Redman', :album => 'Blessed Be Your Name', :tags => 'genuine, seeking, heart'},
+  	  {:title => 'God Is Able', :artist => 'Hillsong United', :album => 'God Is Able', :tags => 'powerful, hope, with us'},
+  	  {:title => 'Commission My Soul', :artist => 'Citipointe Live', :album => 'Commission My Soul: Present', :tags => 'evangelism, purpose, great commission'}
   	 ]
-
-
+  	 
+  	 
 songs.each do |song|
   Songs.create!(song)
 end
