61099e1b3983d5aea127bafc63e5a460e18975b0
James Han <james.han92@berkeley.edu>
2013-11-08 13:53:54 -0800

    merge

--- app/models/songs.rb
+++ app/models/songs.rb
@@ -1,6 +1,6 @@
 class Songs < ActiveRecord::Base
   
-  attr_accessible :title, :artist, :album, :tags
+  attr_accessible :title, :artist, :album, :tags, :file
   
   #auto_index false for now for testing
   searchable :auto_index => false do 
