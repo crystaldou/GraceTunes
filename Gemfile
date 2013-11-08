1e6698113c087cebed22356319a951e2c1aed7d2
Dasol Jung <dasol.jung@gmail.com>
2013-11-07 22:14:38 -0800

    started uploading process, added a add page

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ end
 
 gem 'jquery-rails'
 gem 'haml'
+gem 'carrierwave'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131019224347) do
+ActiveRecord::Schema.define(:version => 20131108005943) do
 
   create_table "songs", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20131019224347) do
     t.string   "tags"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "file"
   end
 
 end
