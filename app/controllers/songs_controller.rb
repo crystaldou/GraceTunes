55d78a8c5fdbb1ba18ed63bffb460af04d6eec22
James Han <james.han92@berkeley.edu>
2013-11-18 16:23:35 -0800

    trying pg_search

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -78,7 +78,7 @@ class SongsController < ApplicationController
       elsif @type == "Tags"
         @songs = Songs.where("tags LIKE '%#{@text.downcase}%'")
       elsif @type == "Lyrics"
-        if !ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql"
+        if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "pg"
           @songs = Songs.search_lyrics(@text.downcase)
         else
           @songs = Songs.where("lyrics LIKE '%#{@text.downcase}%'")
