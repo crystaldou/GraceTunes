694e29b8bf679d83ca14fe8d854cc89b487b3135
Crystal Dou <Crystal@Crystals-MacBook-Pro.local>
2013-11-23 16:39:20 -0800

    fixed merging

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -1,21 +1,24 @@
 class PlaylistController < ApplicationController
   # viewing all playlists
   def index
-    if not current_user.nil?
+    if current_user != nil
       @playlists = Playlist.where(:user_id => current_user.uid)
     else
       @playlists = Playlist.all
     end
+    @playlists = Playlist.all
+    @song_preview = {}
     @playlists.each do |playlist|
       @song_preview[playlist] = ""
       playlist.songss.each do |song|
         @song_preview[playlist] << song.title << ", "
       end
-      @song_preview[playlist].slice!(-1,-3)
-      @song_preview[playlist][0,50]
+      @song_preview[playlist] = @song_preview[playlist][0..-3] + " "
+      @song_preview[playlist] = @song_preview[playlist][0,50]
       @song_preview[playlist] << "..."
     end
   end
+  
 
   def create
     @playlists = Playlist.create!(params[:playlist])
