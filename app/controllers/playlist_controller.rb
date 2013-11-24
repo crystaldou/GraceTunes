5d7e51cc60783e96c126b0a20a4ac5e1973ef793
Gerald Park <geraldpark7@gmail.com>
2013-11-23 16:15:22 -0800

    Playlist view for showing a partcular playlist is ready

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -4,4 +4,20 @@ class PlaylistController < ApplicationController
     @playlists = Playlist.all
   end
 
+  def show
+    id = params[:id]
+    @playlist = Playlist.find(id)
+    
+    @songs = @playlist.songss
+    
+    @tags = {}
+    @songs.each do |song|
+      begin
+        @tags[song] = song.tags.split(", ")
+      rescue
+        @tags[song] = []
+      end
+    end
+  end
+
 end
