608e53307161db9f996ced38cea8b7d44e3bbada
James Han <james.han92@berkeley.edu>
2013-12-10 03:20:47 -0800

    improve code climate

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -17,20 +17,7 @@ class PlaylistController < ApplicationController
     end
   end
   
-  def createPreview playlist
-    @song_preview[playlist] = ""
-    playlist.songss.each do |song|
-      @song_preview[playlist] << song.title << ", "
-    end
-    @song_preview[playlist] = @song_preview[playlist][0...-2]
-    if @song_preview[playlist].length > 150
-      @song_preview[playlist] = @song_preview[playlist][0,150]
-      if @song_preview[playlist][max_length] == ','
-        @song_preview[playlist] = @song_preview[playlist][0...150]
-      end
-      @song_preview[playlist] << "..."
-    end
-  end
+
   def new
     if not current_user.try(:admin?)
       render :template => "/errors/unauthorized.html.haml"
